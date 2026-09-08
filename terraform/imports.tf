eef299b9b889da7bb2ab5302f137c0693d66a87c
rsmith949 <smithray949@gmail.com>
2026-09-07 23:07:47 -0700

    Untrack saved plan and import blocks

--- terraform/imports.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_instance.service_watch
-  id = "i-0b62f43aaba65c25c"
-}
-
-import {
-  to = aws_sns_topic_subscription.alerts_email
-  id = "arn:aws:sns:us-west-2:182715287298:service-watch-alerts:1052c6bc-5b80-4ade-bf4d-8669a1a4ef53"
-}
